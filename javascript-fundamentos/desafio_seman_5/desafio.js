c10916e7e59cd82e787ce916c347dd40d0927803
etivaldosouza <etivaldosouza@hotmail.com>
2025-03-31 20:45:16 -0300

    Create desafio.js

---
+++ javascript-fundamentos/desafio_seman_5/desafio.js
@@ -0,0 +1,10 @@
+/*  
+
+Escreva uma função que receba um array de números e retorne a soma apenas dos números pares.
+
+*/
+var num = [1, 2, 3, 4, 5, 6]
+function somar_pares(){
+    var c = 0
+    var s = num[c]
+}
