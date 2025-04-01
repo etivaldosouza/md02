c132d8c5fc4119ee8bc2ab5b2ad4b257c41b7fc7
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-01 14:59:47 -0300

    Update desafio.js

--- javascript-fundamentos/desafio_seman_5/desafio.js
+++ javascript-fundamentos/desafio_seman_5/desafio.js
@@ -3,8 +3,14 @@
 Escreva uma função que receba um array de números e retorne a soma apenas dos números pares.
 
 */
-var num = [1, 2, 3, 4, 5, 6]
-function somar_pares(){
-    var c = 0
-    var s = num[c]
+
+
+function somar_pares(num){
+    var num = [1, 2, 3, 4, 5, 6]
+    if(let c in num % 2 == 0 ){
+        return c + num[c]
+    }
+
 }
+let soma = somar_pares(num[c])
+console.log(soma)
