07b57fe86ba8f8384bce503ad224884158021fda
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-04 19:17:14 -0300

    Update teste.js

--- javascript-fundamentos/desafio_seman_5/teste.js
+++ javascript-fundamentos/desafio_seman_5/teste.js
@@ -14,3 +14,23 @@ function encontrarVogais(palavra) {
 // Exemplo de uso
 const resultado = encontrarVogais("Luzia");
 console.log(`Vogais encontradas: ${resultado.join(', ')}`);
+
+
+//=============///
+
+let valores = [8,1,7,4,2,9]
+valores.sort()
+console.log(valores)
+for(let c = 0; c < valores.length; c++){ // codigo tradicional p/ percurso em vetor
+    console.log(`na posição ${c} está o valor ${valores[c]}`)
+}
+
+//=====
+let valores = [8,1,7,4,2,9]
+valores.sort()
+
+for(let c in valores){ //maneira simplificadsa de percorrer um vetor(só funciona p/ array e objetos)
+    console.log(`na posição ${c} está o valor ${valores[c]}`)
+}
+
+
