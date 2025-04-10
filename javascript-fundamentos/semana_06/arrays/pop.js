3e16ac6573ab9be12a73703f2ec1e2b20e49073a
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-10 16:20:26 -0300

    ok

---
+++ javascript-fundamentos/semana_06/arrays/pop.js
@@ -0,0 +1,5 @@
+// metodo pop = remove o ultimo elemento do array e retorna o ultimo elemento removido.
+
+const numeros = [10,6,8,19,18,20]
+const ordem = numeros.sort()
+console.log(ordem)
---
+++ javascript-fundamentos/semana_06/arrays/push.js
@@ -0,0 +1,4 @@
+const numeros = [10,20]
+const arrayTotal = numeros.push(30,40) // joga esses nrs p/ o final do array
+console.log(numeros)
+console.log(arrayTotal)
