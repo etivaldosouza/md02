4c24b16f6fdb08a9ae13b813faa6fef49de57e6c
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-11 11:28:19 -0300

    metodo map

--- javascript-fundamentos/semana_06/arrays/map.js
+++ javascript-fundamentos/semana_06/arrays/map.js
@@ -1,7 +1,54 @@
-//metodo maps: 
+//metodo maps: utilizado para manipular um array e retornar um resultado de um novo array com o valor do primeiro array modificado.
+
+// o map ele usa uma funcao de calback como parametro e essa funcao  vai manipular  cada valor desse array entao esse map iterar esse valor desse array um a um e ele vai aplicar em cada valor a funcao que a gente vai passar.
+
+// alterando um array de numeros e criando um novo array formado  pelo array antigo * 2 
+
+// utilizando o loop for normal: 
+
+const numeros = [2,3,6,5,10]
+let novoArray = []
+for(let c = 0; c < numeros.length;c++) {
+    novoArray.push(numeros[c] *2 )
+}
+
+console.log(numeros)
+console.log(novoArray)
+
+//============/ /================/ /===================
+
+// utilizando o metodo MAP: ñ precisa ficar cirando variavel fora do escopo para alterar depois como ocorreu no caso acima. tive que criar uma variavel com um arrayNovo fora do for.
+
+// o map é um metodo de array. é uma function que espera que vc passe uma funcao de callback p/ ela. essa calback é p/ manipular cada valor desse array. entao esse map vai iterar os valores desse array um a um  aplicando a função que vc passar.
 
 const numeros = [2,3,6,5,10]
 
-for(let c = 0  of numeros) {
-    console.log(c)
-}
+numeros.map(function(numero){ // função map recep uma função de callback que percorre cada elemento do array
+    console.log(numero)
+})
+
+//==========/ /================/ /============/ /==========
+// criar um array novo com o dobro dos elemento do array antigo
+
+const numeros = [2,3,6,5,10]
+
+const novoArray = numeros.map(function(numero){
+    return numero * 2
+})
+
+console.log(numeros) // imprime array antigo
+console.log(novoArray) // imprime  array nobo
+
+//=========/ /===============/ /======================/ /=====
+// usando arrow function(maneira simplificada)
+
+const numeros = [2,3,6,5,10]
+
+const novoArray = numeros.map(numero => numero * 2)
+console.log(novoArray)
+
+
+
+
+
+
---
+++ javascript-fundamentos/semana_06/arrays/map01.js
@@ -0,0 +1,17 @@
+// entendendo o metodo map: vai invocar uma função callback passada por argumento para cada elemento do array e ele vai retornar um novo array com os elementos retornados pela sua funcao.
+
+const numeros = [2,3,6,5,10]
+
+const numerosDobro = numeros.map(dobro) // double é a funcao de callbak q to passando
+
+/*function dobro(element,index,array){
+    console.log(element,index,array)
+}
+*/
+
+function dobro(element){
+    return element * 2
+}
+
+console.log('ORIGINAL',numeros)
+console.log('NOVO ARRAY',numerosDobro)
