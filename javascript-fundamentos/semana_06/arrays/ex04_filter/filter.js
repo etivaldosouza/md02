0e49470f73f7f0555babf607dc7c9b26aae30778
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-14 17:11:34 -0300

    criado exercicios map ,filter, reduce

--- javascript-fundamentos/semana_06/arrays/ex04_filter/filter.js
+++ javascript-fundamentos/semana_06/arrays/ex04_filter/filter.js
@@ -1,4 +1,4 @@
-//filter : criar um novo array mas filtrando fora alguns desses itens. os elementos que vão para o novo array vao fazer parte desse array respeitando um parametro
+//filter : criar um novo array mas filtrando fora alguns desses itens. os elementos que vão para o novo array, vao fazer parte desse array respeitando um parametro passado por mim.
 
 const produtos = [
     
@@ -9,3 +9,21 @@ const produtos = [
     {id: 5, nome:'leite', valor: 2.20, categoria:'alimento'}
 ]
 
+// combinando map e filter. criando um array de produtos de categoria alimento e com o nome dos produtos
+
+const alimentos = produtos.filter(p => p.categoria === 'alimento').map(p => p.nome)
+
+console.log(alimentos)
+
+// ou :
+
+const produtos = [
+    
+    {id: 1, nome: 'detergente', valor: 2.00, categoria: 'limpeza'},
+    {id: 2, nome:'amaciante', valor: 6.50, categoria:'limpeza'},
+    {id: 3, nome:'pão', valor: 2.00, categoria:'alimento'},
+    {id: 4, nome:'queijo', valor: 7.00, categoria:'alimento'},
+    {id: 5, nome:'leite', valor: 2.20, categoria:'alimento'}
+]
+
+console.log(produtos.filter(p => p.categoria === 'alimento').map(p => p.nome)) 
---
+++ javascript-fundamentos/semana_06/arrays/ex05_reduce/reduce.js
@@ -0,0 +1,29 @@
+// com o reduce podemos gerar um único valor atraves de uma coleção(lista ou array). por exemplo quando temos um array de numeros e queremos somar todos os valores desse array e obter um valor total.
+
+// no reduce a função de callback vai receber dois parametros: um é o valor acumulado e o outro é o item do array
+
+// calculando o valor todal de um array:
+
+const numeros = [1,2,3,4]
+
+const total = numeros.reduce(function(valorAcumulador,valorArray){
+    return valorAcumulador + valorArray
+},0)
+
+console.log(total)
+
+//===========/ /==============/ /===========/ /====================
+
+
+const produtos = [
+    
+    {id: 1, nome: 'detergente', valor: 2.00, categoria: 'limpeza'},
+    {id: 2, nome:'amaciante', valor: 6.50, categoria:'limpeza'},
+    {id: 3, nome:'pão', valor: 2.00, categoria:'alimento'},
+    {id: 4, nome:'queijo', valor: 7.00, categoria:'alimento'},
+    {id: 5, nome:'leite', valor: 2.20, categoria:'alimento'}
+] 
+
+const total = produtos.reduce((valorAcumulador,produto) => valorAcumulador + produto.valor,0)
+
+console.log(total)
