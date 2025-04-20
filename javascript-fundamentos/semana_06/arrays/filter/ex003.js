3ef21126ada9fbdbcdfc6e403886b110584763ea
etivaldosouza <etivaldosouza@hotmail.com>
2025-04-20 12:19:19 -0300

    Create ex003.js

---
+++ javascript-fundamentos/semana_06/arrays/filter/ex003.js
@@ -0,0 +1,11 @@
+/*
+    filter() usado quando é preciso remover elementos indesejados com base em algumas condicoes. ou seja, o filter() é utilizado quando é preciso filtrar
+
+     a sintaxe é a mesma do map, a diferença para é que na função de callback precisa retornar um valor boleano:
+
+*/
+
+// utilizando o filter() para remover elementos duplicados em um array:
+
+const numeros = [2,3,4,5,4,12,19,7,2]
+
