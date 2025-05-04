81e1ce5990c6a57e7e85ed8ce0d1b7c585bd93ad
etivaldosouza <etivaldosouza@hotmail.com>
2025-05-03 22:29:28 -0300

    Update filtrando_produtos_caros.js

---
+++ javascript-fundamentos/semana_06/desafio_semana_06/filtrando_produtos_caros.js
@@ -0,0 +1,19 @@
+// crie um novo array onde todos os produtos tenham um desconto de 10% aplicado.
+
+
+const produtos = [
+    {
+        nome: 'notebook',
+        preco: 3000
+    },
+
+    {
+        nome: 'Smartphone',
+        preco: 2000
+    },
+
+    {
+        nome: 'tablet',
+        preco: 1500
+    }
+]
