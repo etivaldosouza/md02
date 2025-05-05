a40608ea33b9bcc1fbfac464f5a794767facac9e
etivaldosouza <etivaldosouza@hotmail.com>
2025-05-05 08:11:17 -0300

    Create ajustando_preco_desconto.js

---
+++ javascript-fundamentos/semana_06/desafio_semana_06/ajustando_preco_desconto.js
@@ -0,0 +1,35 @@
+/*
+    encontrar todos os produtos que custam mais de R$ 1.600.
+*/
+
+const produtos = [
+    {
+        nome: 'Notebook',
+        preco: 3000
+
+    },
+
+    {
+        nome: 'Smartphone',
+        preco: 2000
+
+    },
+
+    {
+        nome: 'Tablet',
+        preco: 1500
+
+    },
+
+    {
+        nome: 'Fone de Ouvido',
+        preco: 1800
+
+    },
+
+    {
+        nome: 'Caneta',
+        preco: 8000
+
+    },
+]
