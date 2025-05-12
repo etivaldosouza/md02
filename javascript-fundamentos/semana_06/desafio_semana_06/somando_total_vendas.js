e3cbeb4f5b35adaa5b134af58c7016139f8dc79b
etivaldosouza <etivaldosouza@hotmail.com>
2025-05-12 19:06:49 -0300

    Update somando_total_vendas.js

--- javascript-fundamentos/semana_06/desafio_semana_06/somando_total_vendas.js
+++ javascript-fundamentos/semana_06/desafio_semana_06/somando_total_vendas.js
@@ -1,3 +1,13 @@
 // calcule o total de faturamento de uma loja
 
-const vendas = 
+const vendas = [
+    {Produtos: 'Notebook', valor: 3000 },
+    {Produtos: 'Smartphone', valor: 2000 },
+    {Produtos: 'Tablet', valor: 1500 }
+]
+
+const faturamento = vendas.reduce(function(acc,valorAtual){
+    return acc + valorAtual.valor
+},0)
+
+console.log(faturamento)
