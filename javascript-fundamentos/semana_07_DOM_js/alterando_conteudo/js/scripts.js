9d2aec5653895f3d3af6a8d306894dac19d2973c
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-05-14 20:34:29 -0300

    atividade sobre querySelector

---
+++ javascript-fundamentos/semana_07_DOM_js/alterando_conteudo/js/scripts.js
@@ -0,0 +1,27 @@
+/*
+ 
+    formas de alterar conteudo do elemento
+
+    1) innerHTML
+
+    2) textContent -> mais utilizada, recomendada e performatica
+
+
+ */
+
+
+// innerHTML
+
+var titulo = document.querySelector('#title')
+
+titulo.innerHTML = 'Testando o texto alterado'
+
+
+// textContent
+
+ var subtitulo = document.querySelector('.subtitle')
+ console.log(subtitulo)
+ 
+ subtitulo.textContent = 'Testando a alteração do subtitulo'  
+
+
