9b4cc350548ac7cc11eb0f10dfecc77f2284412a
etivaldosouza <etivaldosouza@hotmail.com>
2025-05-14 15:10:29 -0300

    exercicio sobre dom

---
+++ javascript-fundamentos/semana_07_DOM_js/acessando_DOM/js/scripts.js
@@ -0,0 +1,39 @@
+// acessando por:
+
+// tag: 
+
+
+
+var titulo = document.getElementsByTagName('h1')[0] // uso do plural pq pode ser mais de uma tag h1
+
+console.log(titulo)
+
+
+var lis = document.getElementsByTagName('li')
+
+console.log(lis)
+
+
+//================//
+
+// por id : 
+
+// como o id só pode ter um por pagina, diferente de class que posso ter mais de uma class na mesma pagina como foi o caso da class 'item' o id será sempre com o singular(Bytagname)
+
+
+var paragrafo = document.getElementById('paragrafo')
+
+console.log(paragrafo)
+
+
+//===================//
+
+// por class :
+
+
+var itensDaLista = document.getElementsByClassName('item')
+
+console.log(itensDaLista)
+
+
+// formas mais moderna (querySelector e querySelectorAll)
---
+++ javascript-fundamentos/semana_07_DOM_js/querySelector_querySelectorAll/js/scripts.js
@@ -0,0 +1,8 @@
+var itensQuery = document.querySelectorAll('#lista  .item')
+
+console.log(itensQuery)
+
+
+var itensQuery2 = document.querySelectorAll('#lista2 li')
+
+console.log(itensQuery2)
