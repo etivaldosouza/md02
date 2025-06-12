d6f1351125d4440176eaa6a839933c30fa400316
EtivaldoSouza <etivaldosouza@hotmail.com>
2025-06-12 16:56:17 -0300

    adicionado semana 08

---
+++ javascript-fundamentos/semana_07_DOM_js/getAttribute()/js/script.js
@@ -0,0 +1,11 @@
+// quando quero pegar um atributo dos elementos (getAttribute)
+
+let dv = document.querySelector('div')
+
+dv.getAttribute('id') // verifica qual o id do elemento se tiver
+console.log(dv) // vai mostrar o nome do atributo id da div
+
+// dv.removeAttribute('id') (se eu quiser remover o atributo id) 
+
+
+
---
+++ javascript-fundamentos/semana_08_desafio lista dinamica/js/scripts.js
@@ -0,0 +1,10 @@
+const tarefas = document.querySelector('#tarefas')
+const lista = document.querySelector('#lista-tarefas')
+const butao = document.querySelector('#butao')
+
+butao.addEventListener('click','adicionarTarefas')
+
+function adicionarTarefas(){
+    const text = tarefas.value.trim()
+    if(text ===)
+}
